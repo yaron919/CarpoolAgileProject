e4f96b97a0d0c3004ed4cf3aa7ff185110030c39
IdoShapira <ishapira@yotpo.com>
2018-05-15 16:07:24 +0300

    Finlization for registration process

--- app/src/main/java/com/afeka/agile/carpoolagileproject/RegistrationActivity.java
+++ app/src/main/java/com/afeka/agile/carpoolagileproject/RegistrationActivity.java
@@ -44,15 +44,16 @@ public class RegistrationActivity extends AppCompatActivity {
                 String textName = name.getText().toString();
                 validData = !textName.isEmpty();
                 String textAge = age.getText().toString();
-                validData = DataCheck.confirmAge(textAge);
+                validData &= DataCheck.confirmAge(textAge);
                 String textCarModel = carModel.getText().toString();
                 String textSeatNumber = seatNumber.getText().toString();
-                validData = DataCheck.confirmSeatNumber(textSeatNumber);
+                validData &= DataCheck.confirmSeatNumber(textSeatNumber);
                 String textUserName= userName.getText().toString();
-                //TODO validate user name
                 String textPassword = password.getText().toString();
-                DatabaseReference database = FirebaseDatabase.getInstance().getReference();
-                addUser(textName,textAge,textCarModel,textSeatNumber,textPassword,textUserName);
+                if(validData){
+                    addUser(textName,textAge,textCarModel,textSeatNumber,textPassword,textUserName);
+                }else
+                    Toast.makeText(getApplicationContext(),R.string.unValidData,Toast.LENGTH_LONG).show();
 
             }
         });
