b0f6f4743f3b4cd404ab427a59ffa2daf1acf6bf
Yaron Ashual <yaron919@gmail.com>
2018-05-29 17:19:51 +0300

    fix build and more

--- app/src/main/java/com/afeka/agile/carpoolagileproject/JoinCarpoolActivity.java
+++ app/src/main/java/com/afeka/agile/carpoolagileproject/JoinCarpoolActivity.java
@@ -3,6 +3,7 @@ package com.afeka.agile.carpoolagileproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,33 +11,16 @@ import android.widget.Toast;
 
 public class JoinCarpoolActivity extends AppCompatActivity {
 
-    private Intent intentToMain;
-
+    private RecyclerView rides;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_carpool);
-        final EditText time = findViewById(R.id.join_time);
-        final EditText date = findViewById(R.id.join_date);
-        final EditText location = findViewById(R.id.join_location);
-        Button confirmButton = findViewById(R.id.join_confirm_b);
+        rides = (RecyclerView) findViewById(R.id.rides_recycle_view);
 
-        intentToMain = new Intent(this,MainMenuActivity.class);
 
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                /** TO DO - ADD DB FireBase connection **/
-                String textTime = time.getText().toString();
-                String textDate = date.getText().toString();
-                String textLocation = location.getText().toString();
 
-                Toast.makeText(getApplicationContext(),R.string.join_sent_request,Toast.LENGTH_LONG).show();
-
-                startActivity(intentToMain);
-            }
-        });
 
     }
 }
